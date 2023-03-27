edfc042ad383c7f900eb91242d02f3df94c4c09b
Chris Gjuraj <cgjuraj@gmail.com>
2023-03-27 13:34:56 -0400

    backend adjustments

--- movies/src/main/java/dev/chris/movies/Review.java
+++ movies/src/main/java/dev/chris/movies/Review.java
@@ -8,6 +8,8 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
+
 @Document(collection = "reviews")
 @Data
 @AllArgsConstructor
@@ -17,8 +19,12 @@ public class Review {
     @Id
     private ObjectId id;
     private String body;
+    private LocalDateTime created;
+    private LocalDateTime updated;
 
-    public Review(String body) {
+    public Review(String body, LocalDateTime created, LocalDateTime updated) {
         this.body = body;
+        this.created = created;
+        this.updated = updated;
     }
 }
--- movies/src/main/java/dev/chris/movies/ReviewService.java
+++ movies/src/main/java/dev/chris/movies/ReviewService.java
@@ -6,6 +6,8 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 public class ReviewService {
 
@@ -15,7 +17,7 @@ public class ReviewService {
     private MongoTemplate mongoTemplate;
 
     public Review createReview(String reviewBody, String imdbId) {
-        Review review = reviewRepository.insert(new Review(reviewBody));
+        Review review = reviewRepository.insert(new Review(reviewBody, LocalDateTime.now(), LocalDateTime.now()));
 
         mongoTemplate.update(Movie.class).matching(Criteria.where("imdbId").is(imdbId)).apply(new Update().push("reviewIds").value(review)).first();
         //update movie                            //matches ID from user                               //push update review array           //updating single movie
