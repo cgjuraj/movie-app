9e48d2960d7e14dc4f0d300015698dcc7a6375a4
Chris Gjuraj <cgjuraj@gmail.com>
2023-03-30 22:57:32 -0400

    adjustment note

--- movies-client/movie-api-v1/src/components/reviews/Reviews.js
+++ movies-client/movie-api-v1/src/components/reviews/Reviews.js
@@ -24,7 +24,7 @@ const Reviews = ({getMovieData, movie, reviews, setReviews}) => {
 
         try
         {
-            const response = await api.post("/api/v1/reviews", {reviewBody:rev.value, imdbId:movieId});
+            const response = await api.post("/api/v1/reviews", {reviewBody:rev.value, imdbId:movieId}); /* adjust here */
 
             const updatedReviews = [...reviews, {body:rev.value}];
 
